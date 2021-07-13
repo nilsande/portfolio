4dffa822a1ca8644f5fb5d3f76062dfe71d756a3
Nils Andersson <flygmottomgylf@gmail.com>
2021-07-13 16:32:28 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -2465,7 +2465,7 @@
 
 	            openInfo.className = 'full-post-mobile-info';
 	            openInfo.setAttribute('data-action', 'full-post-show-info');
-	            openInfo.innerHTML = '&#8679;';
+	            openInfo.innerHTML = 'INFO';
 
 	            mobileMenu.appendChild(openInfo);
 	            mobileMenu.appendChild(socials);
